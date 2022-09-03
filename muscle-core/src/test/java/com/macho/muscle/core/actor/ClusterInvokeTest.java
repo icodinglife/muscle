a8848a3350d0d6f87afc3ef751cc5d992c17b233
hunter <horryq@hotmail.com>
2022-09-04 02:02:33 +0800

    add cluster invoke test

--- muscle-core/src/test/java/com/macho/muscle/core/actor/ClusterInvokeTest.java
+++ muscle-core/src/test/java/com/macho/muscle/core/actor/ClusterInvokeTest.java
@@ -2,8 +2,11 @@ package com.macho.muscle.core.actor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.macho.muscle.core.actor.MuscleSystem.PUBLISH;
@@ -37,5 +40,23 @@ public class ClusterInvokeTest {
         log.info("hello, {}", hello2);
     }
 
+    @Test
+    public void startTest3() throws InterruptedException {
+        MuscleSystem muscleSystem = new MuscleSystem();
+        muscleSystem.startCluster(ETCD_ADDR, 16668);
+
+        TimeUnit.SECONDS.sleep(3);
+
+        List<String> svc1 = muscleSystem.getClusterSystem().getActorIdsWithService("svc1");
+        if (CollectionUtils.isEmpty(svc1)) {
+            throw new RuntimeException("actor list is empty");
+        }
+
+        InvokeTest.ITestInvoker iTestInvoker = muscleSystem.newInvokeProxy(svc1.get(0), "svc1", InvokeTest.ITestInvoker.class, true);
+        String hello = iTestInvoker.hello("hello");
+        log.info("hello, {}", hello);
+        String hello2 = iTestInvoker.hello("hello");
+        log.info("hello, {}", hello2);
+    }
 
 }
